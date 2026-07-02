84c7ec541036bdf599af786c3b60f8b00881f23c
Lastmile04 <thakurdikshant05@gmail.com>
2026-07-02 21:19:50 +0530

    feat: initialize c-ls and adding directory opening with opendir

---
+++ c-ls/ls.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <dirent.h>
+
+int main(int argc, char *argv[])
+{
+    const char *path = (argc>1)? argv[1] :  ".";
+
+    DIR *dir = opendir(path);
+    // printf("%s\n");
+    return 0;
+}
