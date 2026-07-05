6f22e239185391c288c7e9c5f634df608b426041
Lastmile04 <thakurdikshant05@gmail.com>
2026-07-05 17:53:45 +0530

    feat: implemented -a flag using getopt()

--- c-ls/ls.c
+++ c-ls/ls.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <getopt.h>
+#include <sys/stat.h>
+
+// global bool var for differnt option flags
+int show_all = 0;       // show hidden files
+int long_format = 0;    // show long format (sys stat info)
 
 int main(int argc, char *argv[]) 
 {
-    const char *path = (argc>1)? argv[1] :  ".";
+    int opt;
+    while ((opt = getopt(argc, argv, "a")) != -1) {
+        switch(opt){
+            case 'a':
+               show_all = 1;
+               break;
+            case 'l':
+               long_format = 1;
+               break;
+            default:
+               fprintf(stderr, "usage: %s [-a] [path]\n", argv[0]);
+               return 1;
+        }
+    }
+
+    const char *path = (optind < argc)? argv[optind] :  ".";
 
     DIR *dir = opendir(path);
     if(!dir) {
@@ -13,7 +34,9 @@ int main(int argc, char *argv[])
 
     struct dirent *entry;
     while((entry = readdir(dir)) != NULL){
-        if(entry ->d_name[0] == '.') continue; // to skip the dot files  
+        if(!show_all && entry ->d_name[0] == '.') continue; // to skip the dot files  
+        if( long_format)
+            
         printf("%s\n", entry->d_name);  // d_name comes from the dereferencing the struct: (entry*).d_name
     }   
     closedir(dir);
